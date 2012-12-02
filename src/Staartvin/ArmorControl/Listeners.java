e88444cecd9a385e288a03f0ab3269c855d4f238
Vincent <mijnleraar@msn.com>
2012-12-02 16:53:01 +0100

    Added listeners to control weapon and item use. Quite a rewrite. (Not done yet)
    * Added EntityDamageByEntityEvent for weapon control
    * Added BlockBreakEvent for tool control

--- src/Staartvin/ArmorControl/Listeners.java
+++ src/Staartvin/ArmorControl/Listeners.java
@@ -1,8 +1,11 @@
 package Staartvin.ArmorControl;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 
@@ -16,6 +19,7 @@ public class Listeners implements Listener {
 	
 	@EventHandler
 	public void onInventoryOpen(InventoryOpenEvent event) {
+		if (!plugin.getConfig().getBoolean("UseArmorControl")) return;
 		// Player is not online for some reason
 		if (plugin.getServer().getPlayer(event.getPlayer().getName()) == null) return;
 	    Player player = plugin.getServer().getPlayer(event.getPlayer().getName());
@@ -23,11 +27,12 @@ public class Listeners implements Listener {
 	    if (player.hasPermission("armorcontrol.exempt")) return;
 	    
 	    plugin.inv = plugin.getServer().getPlayer(event.getPlayer().getName()).getInventory();
-	    plugin.methods.checkInventory(player); 
+	    plugin.methods.checkInventoryforArmor(player); 
 	}
 	
 	@EventHandler
 	public void onInventoryClose(InventoryCloseEvent event) {
+		if (!plugin.getConfig().getBoolean("UseArmorControl")) return;
 		// Player is not online for some reason
 		if (plugin.getServer().getPlayer(event.getPlayer().getName()) == null) return;
 	    Player player = plugin.getServer().getPlayer(event.getPlayer().getName());
@@ -35,6 +40,66 @@ public class Listeners implements Listener {
 	    if (player.hasPermission("armorcontrol.exempt")) return;
 	    
 	    plugin.inv = plugin.getServer().getPlayer(event.getPlayer().getName()).getInventory();
-	    plugin.methods.checkInventory(player); 
+	    plugin.methods.checkInventoryforArmor(player); 
 	}
+	
+	@EventHandler
+	public void onWeaponUse(EntityDamageByEntityEvent event) {
+		if (!plugin.getConfig().getBoolean("UseWeaponControl")) return;
+		
+		// Damager is not a player for some reason
+		if (!(event.getDamager() instanceof Player)) return;
+	    Player player = (Player) event.getDamager();
+	 // Player has got the correct exempt permission
+	    if (player.hasPermission("weaponcontrol.exempt")) return;
+//	    if (player.getItemInHand() == null) return;
+	    int IDinHand = player.getItemInHand().getTypeId();
+	    // Nothing in hand
+	    if (IDinHand == 0) return;
+	    if (IDinHand == 267 || IDinHand == 268 || IDinHand == 272 || IDinHand == 276 || IDinHand == 283 || IDinHand == 261) {
+		    for (int ID : plugin.weaponIDs) {
+		    	if (IDinHand == ID) {
+		    		if (!plugin.methods.checkLevel(player, plugin.methods.checkWeaponType(ID), "weapon")) {
+		    			event.setCancelled(true);
+		    			player.sendMessage(ChatColor.RED + "You cannot use this weapon! You must be at least level: " + plugin.methods.findLevel(plugin.methods.checkWeaponType(ID), "weapon"));
+		    		}
+		    	}
+		    }
+	    }
+	}
+	
+	@EventHandler
+	public void onToolUse(BlockBreakEvent event) {
+		if (!plugin.getConfig().getBoolean("UseToolControl")) return;
+		
+		// Player is not online for some reason
+		if (event.getPlayer() == null) return;
+	    Player player = event.getPlayer();
+	    // Player has got the correct exempt permission
+	    if (player.hasPermission("toolcontrol.exempt")) return;
+	    int IDinHand = player.getItemInHand().getTypeId();
+	    if (IDinHand == 0) return;
+	    // Tool in hand is a weapon
+	    if (IDinHand == 267 || IDinHand == 268 || IDinHand == 272 || IDinHand == 276 || IDinHand == 283 || IDinHand == 261) return;
+	    
+	    // Wood tools
+		    for (int ID : plugin.woodToolsIDs) {
+		    	if (IDinHand == ID) {
+		    		if (!plugin.methods.checkLevel(player, "wood", "tool")) {
+		    			event.setCancelled(true);
+		    			player.sendMessage(ChatColor.RED + "You cannot use this tool! You must be at least level: " + plugin.methods.findLevel("wood", "tool"));
+		    		}
+		    	}
+		    }
+		    
+		 // Stone tools
+		    for (int ID : plugin.stoneToolsIDs) {
+		    	if (IDinHand == ID) {
+		    		if (!plugin.methods.checkLevel(player, "stone", "tool")) {
+		    			event.setCancelled(true);
+		    			player.sendMessage(ChatColor.RED + "You cannot use this tool! You must be at least level: " + plugin.methods.findLevel("stone", "tool"));
+		    		}
+		    	}
+		    }
+	    }	
 }
