cfa8683839b5e8910cd784b4276db035a71ab075
Vincent <mijnleraar@msn.com>
2013-02-17 18:03:57 +0100

    Added tool check in OnWeaponUse.

--- src/Staartvin/ArmorControl/Listeners.java
+++ src/Staartvin/ArmorControl/Listeners.java
@@ -69,24 +69,8 @@ public class Listeners implements Listener {
 		    		}
 		    	}
 		    }
-	    }
-	}
-	
-	@EventHandler
-	public void onToolUse(BlockBreakEvent event) {
-		if (!plugin.getConfig().getBoolean("UseToolControl")) return;
-		
-		// Player is not online for some reason
-		if (event.getPlayer() == null) return;
-	    Player player = event.getPlayer();
-	    // Player has got the correct exempt permission
-	    if (player.hasPermission("toolcontrol.exempt")) return;
-	    int IDinHand = player.getItemInHand().getTypeId();
-	    if (IDinHand == 0) return;
-	    // Tool in hand is a weapon
-	    if (IDinHand == 267 || IDinHand == 268 || IDinHand == 272 || IDinHand == 276 || IDinHand == 283 || IDinHand == 261) return;
-	    
-	    // Wood tools
+	    } else {
+			// Wood tools
 		    for (int ID : plugin.woodToolsIDs) {
 		    	if (IDinHand == ID) {
 		    		if (!plugin.methods.checkLevel(player, "wood", "tool")) {
@@ -135,5 +119,72 @@ public class Listeners implements Listener {
 		    		}
 		    	}
 		    }
+	    }
+	    
+	}
+	
+	@EventHandler
+	public void onToolUse(BlockBreakEvent event) {
+		if (!plugin.getConfig().getBoolean("UseToolControl")) return;
+		
+		// Player is not online for some reason
+		if (event.getPlayer() == null) return;
+	    Player player = event.getPlayer();
+	    // Player has got the correct exempt permission
+	    if (player.hasPermission("toolcontrol.exempt")) return;
+	    int IDinHand = player.getItemInHand().getTypeId();
+	    if (IDinHand == 0) return;
+	    // Tool in hand is a weapon
+	    if (IDinHand == 267 || IDinHand == 268 || IDinHand == 272 || IDinHand == 276 || IDinHand == 283 || IDinHand == 261) return;
+	    
+		// Wood tools
+	    for (int ID : plugin.woodToolsIDs) {
+	    	if (IDinHand == ID) {
+	    		if (!plugin.methods.checkLevel(player, "wood", "tool")) {
+	    			event.setCancelled(true);
+	    			player.sendMessage(ChatColor.RED + "You cannot use this tool! You must be at least level: " + plugin.methods.findLevel("wood", "tool"));
+	    		}
+	    	}
+	    }
+	    
+	 // Stone tools
+	    for (int ID : plugin.stoneToolsIDs) {
+	    	if (IDinHand == ID) {
+	    		if (!plugin.methods.checkLevel(player, "stone", "tool")) {
+	    			event.setCancelled(true);
+	    			player.sendMessage(ChatColor.RED + "You cannot use this tool! You must be at least level: " + plugin.methods.findLevel("stone", "tool"));
+	    		}
+	    	}
+	    }
+	    
+	 // Iron tools
+	    for (int ID : plugin.ironToolsIDs) {
+	    	if (IDinHand == ID) {
+	    		if (!plugin.methods.checkLevel(player, "iron", "tool")) {
+	    			event.setCancelled(true);
+	    			player.sendMessage(ChatColor.RED + "You cannot use this tool! You must be at least level: " + plugin.methods.findLevel("iron", "tool"));
+	    		}
+	    	}
+	    }
+	    
+	 // Gold tools
+	    for (int ID : plugin.goldToolsIDs) {
+	    	if (IDinHand == ID) {
+	    		if (!plugin.methods.checkLevel(player, "gold", "tool")) {
+	    			event.setCancelled(true);
+	    			player.sendMessage(ChatColor.RED + "You cannot use this tool! You must be at least level: " + plugin.methods.findLevel("gold", "tool"));
+	    		}
+	    	}
+	    }
+	    
+	 // Diamond tools
+	    for (int ID : plugin.diamondToolsIDs) {
+	    	if (IDinHand == ID) {
+	    		if (!plugin.methods.checkLevel(player, "diamond", "tool")) {
+	    			event.setCancelled(true);
+	    			player.sendMessage(ChatColor.RED + "You cannot use this tool! You must be at least level: " + plugin.methods.findLevel("diamond", "tool"));
+	    		}
+	    	}
+	    }
 	    }	
 }
