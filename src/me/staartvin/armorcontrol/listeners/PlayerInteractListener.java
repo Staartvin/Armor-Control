c04edc4e16edd245992c525a27ed6be851f0cad1
Staartvin <mijnleraar@msn.com>
2016-01-21 00:38:03 +0100

    Use Messagehandler.

--- src/me/staartvin/armorcontrol/listeners/PlayerInteractListener.java
+++ src/me/staartvin/armorcontrol/listeners/PlayerInteractListener.java
@@ -1,5 +1,6 @@
 package me.staartvin.armorcontrol.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -8,6 +9,7 @@ import org.bukkit.inventory.ItemStack;
 
 import me.staartvin.armorcontrol.ArmorControl;
 import me.staartvin.armorcontrol.config.ConfigHandler.actionType;
+import me.staartvin.armorcontrol.config.ConfigHandler.message;
 
 public class PlayerInteractListener implements Listener {
 
@@ -19,30 +21,48 @@ public class PlayerInteractListener implements Listener {
 
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
-		
+
+		Player player = event.getPlayer();
+
 		// We do not have to check on this world.
-		if (plugin.getAPI().isDisabledWorld(event.getPlayer().getLocation().getWorld().getName())) return;
-		
+		if (plugin.getAPI().isDisabledWorld(player.getLocation().getWorld().getName()))
+			return;
+
 		ItemStack item = event.getItem();
-		
+
 		// No item in hand, so nothing to check
-		if (item == null) return;
-		
+		if (item == null)
+			return;
+
 		Action action = event.getAction();
-		
+
 		String blockAction = action.toString();
-		
+
 		int requiredLevel = 0;
 		System.out.println("------------------------");
-		
-		for (actionType type: actionType.values()) {
-			
+
+		for (actionType type : actionType.values()) {
+
 			if (blockAction.equalsIgnoreCase(type.toString())) {
 				requiredLevel = plugin.getResManager().getRequiredLevel(item, type);
 				break;
 			}
 		}
-		
+
 		System.out.println("Required level: " + requiredLevel);
+
+		// Required level is 0.
+		if (requiredLevel <= 0)
+			return;
+
+		// Player does not have the sufficient level
+		if (requiredLevel > player.getLevel()) {
+			plugin.getMessageHandler().sendMessage(player,
+					plugin.getConfigHandler().getMessage(message.NOT_ALLOWED_TO_USE, action.toString(), requiredLevel + ""));
+
+			event.setCancelled(true);
+
+			System.out.println("Event is cancelled.");
+		}
 	}
 }
